493f5c1df6402e3ba6fafcdaf2dca6c67c30179f
rohann216 <pentabud123@gmail.com>
2023-02-16 15:21:43 -0800

    script complete

--- grade.sh
+++ grade.sh
@@ -1,5 +1,26 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
-
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+
+if [[ -f ListExamples.java ]] 
+then 
+    echo "ListExamples found"
+else 
+    echo "need file ListExamples.java"
+    exit 1
+fi
+
+cp ListExamples.java ..
+cd ..
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java 2>compile-error.txt
+
+if [[ $? -ne 0 ]]
+then 
+    echo "COMPILATION ERROR:"
+    cat compile-error.txt
+fi
+
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples >runtime-output.txt
+cat runtime-output.txt
